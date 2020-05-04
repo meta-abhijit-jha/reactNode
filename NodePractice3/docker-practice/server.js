ff08f35df481d2f9596f5a95174c11af8d36efa4
meta-abhijit-jha <abhijit.jha@metacube.com>
2020-05-04 19:04:35 +0530

    Assignment of Docker Implementation

---
+++ NodePractice3/docker-practice/server.js
@@ -0,0 +1,13 @@
+'use-strict'
+
+const express = require('express')
+
+const PORT = 8080
+const HOST = '0.0.0.0'
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send("Hello World from Abhijit")
+}).listen(PORT, HOST)
+
+console.log("Running on http://" + HOST + ":" + PORT)
